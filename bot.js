47cb8368252a621731aa9baa5dafb9060c0b3a97
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-03 15:22:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,10 +223,10 @@ if(cmd === `${prefix}emoji-list`){
     const List = message.guild.emojis.map(e => e.toString()).join(" ");
 
     const EmojiList = new Discord.RichEmbed()
-    if(!EmojiList) return message.channel.send("There is no emojis in this server ):")
+    if(!List) return message.channel.send("There is no emojis in this server ):")
     .setTitle("**__Server emojis :__**")
     .setAuthor(message.guild.name, message.guild.iconURL)
-    .setColor("#00ff93")
+    .setColor("#f7abab")
     .setDescription(List)
     message.channel.send(EmojiList);
 
