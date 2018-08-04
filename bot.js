920ede8b6a0f1de55f4a8259ea7719d5887d02be
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-05 01:42:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -416,37 +416,6 @@ client.on('message', async message =>{
 //////
 ///////
 ////////
-client.on('message', async message =>{
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    const coins = require("./coins.json");
-
-  if(!coins[message.author.id]){
-    coins[message.author.id] = {
-      coins: 0
-    };
-  }
-
-  let coinAmt = Math.floor(Math.random() * 15) + 1;
-  let baseAmt = Math.floor(Math.random() * 15) + 1;
-  console.log(`${coinAmt} ; ${baseAmt}`);
-
-  if(coinAmt === baseAmt){
-    coins[message.author.id] = {
-      coins: coins[message.author.id].coins + coinAmt
-    };
-  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-    if (err) console.log(err)
-  });
-  let coinEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#5074b3")
-  .addField("💸", `${coinAmt} coins added!`);
-
-  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
-
-}
 //
 ///
 ////
@@ -845,4 +814,3 @@ function play(guild, song) {
 
 
 client.login(process.env.BOT_TOKEN);
-});
