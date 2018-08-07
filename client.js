2afc3c5a4d16299ef6c5787f3a48d0eaa038136b
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-07 23:12:48 +0300

    Update client.js

--- client.js
+++ client.js
@@ -438,14 +438,13 @@ client.on('message', message =>{
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
     let prefix = '!!';//بتقدر 
-    let sicon = message.author.displayAvatarURL;
-    if(cmd === `${prefix}suggest`) {
+
+if(cmd === `${prefix}suggest`) {
     var suggestMessage = message.content.substring(8)
     let suggestEMBED = new Discord.RichEmbed()
     .setColor(3447003)
     .setTitle("New suggest just added!!")
     .setDescription(`**${suggestMessage}**`)
-    .setThumbnail(sicon)
     .setFooter(`Suggested By : ${message.author.tag}`);
     message.delete().catch(O_o=>{}) 
     let suggests = message.guild.channels.find(`name`, "suggests");
