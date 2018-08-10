f202c96ea6f5d4525e72f189511c8d08cbc2a084
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-10 20:52:45 +0300

    Update client.js

--- client.js
+++ client.js
@@ -747,12 +747,11 @@ client.on('message', message =>{
     if(cmd === `${prefix}mute`) {
         if(!message.guild.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send("you Don't have enough permissions to use this command")
             if(mUser.hasPermission('ADMINISTRATOR')) return message.channel.send("I don't have enough permissions to mute this user!")
-                const permissions = message.guild.text.channels.permissions
-                message.guild.mUser.channels.permissions {
+                message.guild.mUser.channels.permissions 
                     "EMBED_LINKS": false,
                     "SEND_MESSAGES": false,
                     "ADD_REACTIONS": false
-                }
+
                 let embed = new Discord.RichEmbed();
                 .setAuthor(`${sicon}`, mUser.tag)
                 .setDescription('**To unmute him type !!unmute')
