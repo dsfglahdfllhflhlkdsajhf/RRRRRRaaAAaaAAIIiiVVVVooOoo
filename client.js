9c6e82df255d58294be02750200c0b38dc45b6b3
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-12 02:52:44 +0300

    Update client.js

--- client.js
+++ client.js
@@ -4,6 +4,8 @@ const Util = require('discord.js');
 
 const getYoutubeID = require('get-youtube-id');
 
+const weather = require('weather-js');
+
 const fetchVideoInfo = require('youtube-info');
 
 const YouTube = require('simple-youtube-api');
@@ -586,7 +588,7 @@ client.on('message', message => {
 
     if (!language) {
 
-      return message.reply(`**The Correct usage is \`\`\`\nfix!!tra [The word] to [The language]\`\`\``);
+      return message.reply(`**The Correct usage is...** \`\`\`fix\n!!tra [The word] to [The language]\n\`\`\``);
 
     }
     let finalToTrans = toTrans.slice(toTrans.length - toTrans.length, toTrans.length - 2).join(' ');
@@ -797,6 +799,51 @@ client.on('message', message => {
     .setFooter(message.author.displayAvatarURL, message.author.tag)
     .setThumbnail(sicon)
     message.channel.send(AboutEmbed)
+    }
+});
+//
+///
+////
+/////
+//////
+///////
+////////
+client.on('message', message => {
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let prefix = "!!";
+
+    if (cmd === `${prefix}weather`) {
+        weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) { // Make sure you get that args.join part, since it adds everything after weather.
+            if (err) message.channel.send(err);
+
+            // We also want them to know if a place they enter is invalid.
+            if (result.length === 0) {
+                message.channel.send('**Please enter a valid location.**') // This tells them in chat that the place they entered is invalid.
+                return; // This exits the code so the rest doesn't run.
+            }
+
+            // Variables
+            var current = result[0].current; // This is a variable for the current part of the JSON output
+            var location = result[0].location; // This is a variable for the location part of the JSON output
+
+            // Let's use an embed for this.
+            const embed = new Discord.RichEmbed()
+                .setDescription(`**${current.skytext}**`) // This is the text of what the sky looks like, remember you can find all of this on the weather-js npm page.
+                .setAuthor(`Weather for ${current.observationpoint}`) // This shows the current location of the weather.
+                .setThumbnail(current.imageUrl) // This sets the thumbnail of the embed
+                .setColor("#ffdb98") // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
+                .addField('Timezone',`UTC${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
+                .addField('Degree Type',location.degreetype, true)// This is the field that shows the degree type, and is inline
+                .addField('Temperature',`${current.temperature} Degrees`, true)
+                .addField('Feels Like', `${current.feelslike} Degrees`, true)
+                .addField('Winds',current.winddisplay, true)
+                .addField('Humidity', `${current.humidity}%`, true)
+
+                // Now, let's display it when called
+                message.channel.send({embed});
     }
 });
 /*
