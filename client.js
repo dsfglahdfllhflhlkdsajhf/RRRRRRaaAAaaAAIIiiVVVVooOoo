80436c0a05a42b027d7d2784df6e8f515e9b7b95
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-12 03:13:14 +0300

    Update client.js

--- client.js
+++ client.js
@@ -816,32 +816,22 @@ client.on('message', message => {
     let prefix = "!!";
 
     if (cmd === `${prefix}purge`) {
-       async function purge() {
-            message.delete(); // Lets delete the command message, so it doesnt interfere with the messages we are going to delete.
+        let messagecount = parseInt(args[1]) || 1;
 
-            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
-            if (!message.member.roles.find("name", "bot-commander")) { // This checks to see if they DONT have it, the "!" inverts the true/false
-                message.channel.send('You need the \`bot-commander\` role to use this command.'); // This tells the user in chat that they need the role.
-                return; // this returns the code, so the rest doesn't run.
-            }
+        var deletedMessages = -1;
 
-            // We want to check if the argument is a number
-            if (isNaN(args[0])) {
-                // Sends a message to the channel.
-                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>'); //\n means new line.
-                // Cancels out of the script, so the rest doesn't run.
-                return;
-            }
-
-            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
-            console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
-
-            // Deleting the messages
-            message.channel.bulkDelete(fetched)
-                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
-
-        };
-    };
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (m.author.id == bot.user.id) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
 });
 /*
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
