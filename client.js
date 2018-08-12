4808d9700ce63cbceab4359700e45f7da7d9fb6a
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-13 00:13:15 +0300

    Update client.js

--- client.js
+++ client.js
@@ -812,7 +812,7 @@ client.on('message', message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-    let prefix = "!!";
+    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 if (cmd === `${prefix}setPrefix`) {
   if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
   if(!args[0] || args[0 == "help"]) return message.reply("Usage: !prefix <desired prefix here>");
